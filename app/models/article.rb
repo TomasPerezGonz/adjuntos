0cada042bf8b90e498dd271c231ffb4ce2560aee
Brayan Diaz C <brayandiazc@gmail.com>
2023-08-22 20:41:06 -0500

    variante nueva

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   has_one_attached :foto do |attachable|
     attachable.variant :index, resize_to_limit: [200, 200]
+    attachable.variant :show, resize_to_limit: [500, 500]
   end
 end
